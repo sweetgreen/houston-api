d52e5582c620fc36bdae84a79783176a411e4d66
Ian Stanton <ian@astronomer.io>
2020-02-21 07:58:31 -0600

    Add unit tests astronomer/issues#102

--- src/lib/deployments/database/index.unit.test.js
+++ src/lib/deployments/database/index.unit.test.js
@@ -7,14 +7,32 @@ describe("databaseExports.cleanCreator", () => {
     expect(cleaned).toBe(creator);
   });
 
+  test("test if cleanCreator function does not modify a normal username", async () => {
+    const creator = "my-u$3r";
+    const cleaned = databaseExports.cleanCreator(creator);
+    expect(cleaned).toBe(creator);
+  });
+
   test("test if cleanCreator function strips dbserver name from azure db username", async () => {
     const creator = "user@azure-db";
     const cleaned = databaseExports.cleanCreator(creator);
     expect(cleaned).toBe("user");
   });
+  
+  test("test if cleanCreator function strips dbserver name from azure db username", async () => {
+    const creator = "my-u$3r@azure-db";
+    const cleaned = databaseExports.cleanCreator(creator);
+    expect(cleaned).toBe("my-u$3r");
+  });
 
   test("test if getDbServer function gets azure dbserver name from azure db username", async () => {
     const creator = "user@azure-db";
+    const azureDbServer = databaseExports.getDbServer(creator);
+    expect(azureDbServer).toBe("@azure-db");
+  });
+
+  test("test if getDbServer function gets azure dbserver name from azure db username", async () => {
+    const creator = "my-u$3r@azure-db";
     const azureDbServer = databaseExports.getDbServer(creator);
     expect(azureDbServer).toBe("@azure-db");
   });
@@ -24,4 +42,12 @@ describe("databaseExports.cleanCreator", () => {
     const azureDbServer = databaseExports.getDbServer(creator);
     expect(azureDbServer).toBe("");
   });
+
+  test("test if getDbServer function returns empty string for normal user", async () => {
+    const creator = "my-u$3r";
+    const azureDbServer = databaseExports.getDbServer(creator);
+    expect(azureDbServer).toBe("");
+  });
 });
+
+
