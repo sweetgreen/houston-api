b47a368ce9ddbf4eebe4f4604ae74ffaffe55efb
sjmiller609 <sjmiller609@gmail.com>
2020-03-06 16:17:56 -0500

    Fix wait-for-deployment

--- src/resolvers/subscription/deployment-status/index.js
+++ src/resolvers/subscription/deployment-status/index.js
@@ -51,7 +51,7 @@ export async function subscribe(parent, args, { pubsub }) {
     const res = await Promise.resolve(getMetric(releaseName));
     publish({
       deploymentStatus: {
-        result: res && res.result ? res.result[0].value[1] : 0
+        result: res && res.result && res.result.length ? res.result[0].value[1] : 0
       }
     });
   }, pubsub);
