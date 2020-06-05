c651a224db27a7253dad04f95e51da0dc71040d6
Ryan Hamilton <ryan@ryanahamilton.com>
2020-06-05 14:44:49 -0400

    Rename mutation with more apt name, remove unused argument from schema (#310)
    * Rename mutation with more apt name, remove unused argument from schema
    
    * Retain underlying function name

--- src/resolvers/mutation/update-self/index.js
+++ src/resolvers/mutation/update-self/index.js
@@ -1,13 +1,13 @@
 import { pick } from "lodash";
 
 /*
- * Update a User.
+ * Update Self.
  * @param {Object} parent The result of the parent resolver.
  * @param {Object} args The graphql arguments.
  * @param {Object} ctx The graphql context.
  * @return {User} The updated User.
  */
-export default function updateUser(parent, args, ctx, info) {
+export default function updateSelf(parent, args, ctx, info) {
   // The external facing schema is too loose as JSON.
   // For now, we just pluck out any props that are not in this list.
   const data = pick(args.payload, ["fullName"]);
