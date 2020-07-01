0a2d1796355d7301b9b27b0a536e1ff2b476cb90
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2020-07-01 17:20:44 +0300

    sync cascade deletes with prisma1

--- src/migrations/20200625161933_add-on-delete-cascade-prisma2.js
+++ src/migrations/20200625161933_add-on-delete-cascade-prisma2.js
@@ -5,11 +5,64 @@ export async function up(knex) {
     log.debug(
       'Recreate  constraint for RoleBinding_workspaceId_fkey in  "houston$default"."RoleBinding" table'
     );
-
     await trx.raw(
-      'ALTER TABLE ONLY "houston$default"."RoleBinding"\n' +
-        '  DROP CONSTRAINT "RoleBinding_workspaceId_fkey"\n' +
-        ', ADD CONSTRAINT "RoleBinding_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "houston$default"."Workspace"(id) ON UPDATE CASCADE ON DELETE CASCADE;\n'
+      'ALTER TABLE ONLY "houston$default"."RoleBinding" ' +
+        'DROP CONSTRAINT "RoleBinding_workspaceId_fkey"' +
+        ', ADD CONSTRAINT "RoleBinding_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "houston$default"."Workspace"(id) ON UPDATE CASCADE ON DELETE CASCADE;'
+    );
+
+    log.debug(
+      'Recreate  constraint for RoleBinding_deploymentId_fkey in  "houston$default"."RoleBinding" table'
+    );
+    await trx.raw(
+      'ALTER TABLE ONLY "houston$default"."RoleBinding" ' +
+        'DROP CONSTRAINT "RoleBinding_deploymentId_fkey"' +
+        ', ADD CONSTRAINT "RoleBinding_deploymentId_fkey" FOREIGN KEY ("deploymentId")REFERENCES "houston$default"."Deployment"("id") ON DELETE CASCADE ON UPDATE CASCADE;'
+    );
+
+    log.debug(
+      'Recreate  constraint for RoleBinding_serviceAccountId_fkey in  "houston$default"."RoleBinding" table'
+    );
+    await trx.raw(
+      'ALTER TABLE ONLY "houston$default"."RoleBinding" ' +
+        'DROP CONSTRAINT "RoleBinding_serviceAccountId_fkey"' +
+        ', ADD CONSTRAINT "RoleBinding_serviceAccountId_fkey" FOREIGN KEY ("serviceAccountId")REFERENCES "houston$default"."ServiceAccount"("id") ON DELETE CASCADE ON UPDATE CASCADE;'
+    );
+
+    log.debug(
+      'Recreate  constraint for RoleBinding_userId_fkey in  "houston$default"."RoleBinding" table'
+    );
+    await trx.raw(
+      'ALTER TABLE ONLY "houston$default"."RoleBinding" ' +
+        'DROP CONSTRAINT "RoleBinding_userId_fkey"' +
+        ', ADD CONSTRAINT "RoleBinding_userId_fkey" FOREIGN KEY ("userId")REFERENCES "houston$default"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;'
+    );
+
+    log.debug(
+      'Recreate  constraint for Email_userId_fkey in  "houston$default"."Email" table'
+    );
+    await trx.raw(
+      'ALTER TABLE ONLY "houston$default"."Email" ' +
+        'DROP CONSTRAINT "Email_userId_fkey"' +
+        ', ADD CONSTRAINT "Email_userId_fkey" FOREIGN KEY ("userId")REFERENCES "houston$default"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;'
+    );
+
+    log.debug(
+      'Recreate  constraint for LocalCredential_userId_fkey in  "houston$default"."LocalCredential" table'
+    );
+    await trx.raw(
+      'ALTER TABLE ONLY "houston$default"."LocalCredential" ' +
+        'DROP CONSTRAINT "LocalCredential_userId_fkey"' +
+        ', ADD CONSTRAINT "LocalCredential_userId_fkey" FOREIGN KEY ("userId")REFERENCES "houston$default"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;'
+    );
+
+    log.debug(
+      'Recreate  constraint for OAuthCredential_userId_fkey in  "houston$default"."OAuthCredential" table'
+    );
+    await trx.raw(
+      'ALTER TABLE ONLY "houston$default"."OAuthCredential" ' +
+        'DROP CONSTRAINT "OAuthCredential_userId_fkey"' +
+        ', ADD CONSTRAINT "OAuthCredential_userId_fkey" FOREIGN KEY ("userId")REFERENCES "houston$default"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;'
     );
   });
 }
