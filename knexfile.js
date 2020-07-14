d3fd2b3dc4270760e497eba92bbe98ced4201b4b
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2020-07-15 01:05:54 +0300

    fix knex ssl

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,18 @@
 import "dotenv/config";
 import config from "config";
+import parse from "pg-connection-string";
 
 // Grab database configuration.
 const { connection, migrations } = config.get("database");
-
-const sslConn = `${connection}?ssl=true`;
 
 // Export the knex configuration.
 // This needs to be old school export for knex.
 module.exports = {
   client: "postgres",
-  sslConn,
+  connection: {
+    ssl: true,
+    ...parse(connection)
+  },
   migrations: {
     directory: __dirname + "/src/migrations",
     tableName: migrations.tableName,
