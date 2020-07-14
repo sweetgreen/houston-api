692feccf97fd6cdec02327cd8cc993bf00db642c
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2020-07-15 00:09:04 +0300

    fix knex ssl

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,13 @@ import config from "config";
 // Grab database configuration.
 const { connection, migrations } = config.get("database");
 
+const sslConn = `${connection}?ssl=true`;
+
 // Export the knex configuration.
 // This needs to be old school export for knex.
 module.exports = {
   client: "postgres",
-  connection,
+  sslConn,
   migrations: {
     directory: __dirname + "/src/migrations",
     tableName: migrations.tableName,
