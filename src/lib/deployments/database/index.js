f1be797224dda7d3849d2f57337d2ad0e4a37981
Adam Vandover <adam@astronomer.io>
2020-07-17 13:44:12 -0400

    Update src/lib/deployments/database/index.js
    Co-authored-by: Ash Berlin-Taylor <ash_github@firemirror.com>

--- src/lib/deployments/database/index.js
+++ src/lib/deployments/database/index.js
@@ -190,14 +190,8 @@ export async function ensureUser(conn, user, password) {
  */
 export async function ensureSchema(conn, schema, user) {
   // Check if schema exists
-  const result = await conn.raw(
-    `SELECT EXISTS(SELECT schema_name FROM information_schema.schemata WHERE schema_name = '${schema}')`
-  );
-  const exists = first(result.rows).exists;
-  if (!exists) {
-    // Create a new schema, granting the new user access.
-    await conn.raw(`CREATE SCHEMA ${schema} AUTHORIZATION ${user}`);
-  }
+  // Create a new schema, granting the new user access.
+  await conn.raw(`CREATE SCHEMA IF NOT EXISTS ${schema} AUTHORIZATION ${user}`);
 }
 
 /*
