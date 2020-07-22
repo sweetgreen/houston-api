0f5464fbf7e025517b43811edf7212aaa5195bc5
Adam Vandover <adam@astronomer.io>
2020-07-22 11:23:39 -0400

    Implement NATS worker for deployment image update (#380)
    * Implement NATS worker for deployment image update
    
    * Add NATS publish
    
    * Update test

--- src/workers/index.js
+++ src/workers/index.js
@@ -1,8 +1,10 @@
-import DeploymentDeleted from "./deployment-deleted";
 import DeploymentCreated from "./deployment-created";
+import DeploymentDeleted from "./deployment-deleted";
+import DeploymentImageUpdate from "./deployment-image-update";
 
 // Define Workers
 export default {
+  DeploymentCreated,
   DeploymentDeleted,
-  DeploymentCreated
+  DeploymentImageUpdate
 };
